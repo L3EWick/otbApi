ad686f0f9b7474f0c5922554707d37eab85d0776
Ismael Lima <79029743+L3EWick@users.noreply.github.com>
2024-09-27 13:37:00 -0300

    otb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/png" href="{{asset('img/uc-logo256.png')}}">
     <title>
 
-GBP
+OTB
     </title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
